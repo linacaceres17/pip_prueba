d73a12c4b9dd711d455c745a7582c06a4728b89c
linacaceres17 <linchan17@gmail.com>
2023-01-27 16:54:17 -0500

    first commit

---
+++ venv/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI 
+
+app= FastAPI()
+
+@app.get("/")
+async def root():
+    return "Hola mundo"
+
+@app.get("/url")
+async def root():
+    return {"url": "https://google.com"}
